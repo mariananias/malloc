6f11a28f4c46ae26fbbfd7443f58c2fe234c1f35
mariananias <157740466+mariananias@users.noreply.github.com>
2024-10-02 14:07:32 -0500

    Add files via upload

---
+++ power-test.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int roundUpPower2(int i) {
+  int original = i;
+  while (i >= 2) {
+    if (i == 2) {
+      return original;
+    } else
+      i > 2
+  }
+}
